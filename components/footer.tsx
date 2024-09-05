f2f2e6a544f526cfa8e3ed941692d4be77d74fde
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-09-05 17:06:49 +0300

    Commented unused parts in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="p-6 bg-gray-800 text-center">
-      <ul className="flex justify-center space-x-6 mb-4">
+      {/* <ul className="flex justify-center space-x-6 mb-4">
         <li>
           <a href="#" className="hover:text-gray-300">
             About
@@ -40,8 +40,8 @@ const Footer: React.FC = () => {
         <a href="#">
           <img src="/facebook-icon.png" alt="Facebook" className="w-6 h-6" />
         </a>
-      </div>
-      <p className="text-gray-500">&copy; 2023 Pupify</p>{' '}
+      </div> */}
+      <p className="text-gray-500">&copy; 2024 Dog3</p>{' '}
     </footer>
   );
 };
